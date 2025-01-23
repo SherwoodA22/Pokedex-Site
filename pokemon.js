02f85c300f397ef8c9aff3b94d99025280afe4a8
Alek Sherwood <amanalek@gmail.com>
2025-01-23 09:35:01 -0500

    Added the basic pokemon display.
    Fetched the pokemon name and ids and displayed them in numerical order.

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-const MAX_POKEMON = 1000
+const MAX_POKEMON = 1025
 const listWrapper = document.querySelector(".list-wrapper");
 const searchInput = document.querySelector("#search-input");
 const numberFilter = document.querySelector("#number");
@@ -11,4 +11,53 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
 .then((response) => response.json())
 .then((data) => {
     allPokemons = data.results;
-})
+    displayPokemon(allPokemons);
+});
+
+//Fetching the pokemon names and ids from the PokeApi.
+async function fetchPokemonDataBeforeRedirect(id) {
+   try {
+        const [pokemon, pokemonSpecies] = await Promise.all([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((result) => {
+            result.json();
+        }),
+        fetch(`https://pokeapi.co/api/v1/pokemon-species/${id}`).then((result) => {
+            result.json();
+        }),
+    ])
+    return true;
+   } catch(error) {
+        console.error("Failed to Fetch Pokemon");
+   }
+}
+
+function displayPokemon(pokemon) {
+    listWrapper.innerHTML = "";
+
+    //Creates an element for each of the pokemon to display them.
+    pokemon.forEach((pokemon) => {
+        const pokemonID = pokemon.url.split("/")[6];
+        const listItem = document.createElement("div");
+        listItem.className = "list-item";
+        listItem.innerHTML = `
+        <div class="number-wrape">
+            <p class="caption-fonts">#${pokemonID}</p>
+        </div>
+         <div class="img-wrape">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png" alt="${pokemon.name}"/>
+        </div>
+         <div class="name-wrape">
+            <p class="body3-fonts">#${pokemon.name}</p>
+        </div>
+        `;
+
+        //Will change to the individual pokemon's details page on click.
+        listItem.addEventListener("click", async () => {
+            const success = await fetchPokemonDataBeforeRedirect(pokemonId);
+            if(success){
+                window.location.href = `./detail/html?id=${pokemonID}`;
+            }
+        });
+
+        listWrapper.appendChild(listItem);
+    });
+}
